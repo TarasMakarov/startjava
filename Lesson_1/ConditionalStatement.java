c9190e55dab57886df9da25fa7383fb84eb794fd
Taras Makarov <tarmakar@gmail.com>
2020-06-28 10:09:54 +0300

    implements HW_2

--- Lesson_1/ConditionalStatement.java
+++ Lesson_1/ConditionalStatement.java
@@ -1,23 +1,27 @@
 public class ConditionalStatement {
 	public static void main(String[] args) {
 		int age = 21;
-		boolean sexMale = true;
-		double height = 2.00;
-		char firstLetterName = 'I';
 		if (age > 20) {
 			System.out.println("Пенсия близко, братан!");
 		}
+
+		boolean sexMale = true;
 		if (sexMale) {
 			System.out.println("Отдай ребро!");
 		}
+
 		if (!sexMale) {
 			System.out.println("Хороша, чертовка!");
 		}
+
+		double height = 2.00;
 		if (height < 1.8) {
 			System.out.println("Привет, Фродо!");
 		} else {
 			System.out.println("Привет, Гэндальф!");
 		}
+
+		char firstLetterName = 'I';
 		if (firstLetterName == 'M') {
 			System.out.println("Не может быть!");
 		} else if (firstLetterName == 'I') {
