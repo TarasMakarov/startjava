6dd866baf69103fc5a44b0b3d033a651ac8e7d4e
Taras Makarov <tarmakar@gmail.com>
2020-06-30 14:41:12 +0300

    implements HW_4

--- Lesson_1/GuessNumber.java
+++ Lesson_1/GuessNumber.java
@@ -1,13 +1,13 @@
 public class GuessNumber {
 	public static void main(String[] args) {
-		int winNumber = 38;
+		int compNumber = 38;
 		int attempt = 0;
-		while (attempt != winNumber) {
-			if (winNumber < attempt) {
+		
+		while (attempt != compNumber) {
+			if (compNumber < attempt) {
 				System.out.println("Введенное вами число больше того, что загадал компьютер");
 				attempt--;
-			} 
-			if (winNumber > attempt) {
+			} else {
 				System.out.println("Введенное вами число меньше того, что загадал компьютер");
 				attempt++;
 			}
@@ -15,27 +15,3 @@ public class GuessNumber {
 		System.out.println("Вы угадали!");
 	}
 }
-
-// 		attempt = attempt + 73;
-// 		if (winNumber < attempt) {
-// 			System.out.println("Введенное вами число больше того, что загадал компьютер");
-// 		} 
-// 		if (winNumber > attempt) {
-// 			System.out.println("Введенное вами число меньше того, что загадал компьютер");
-// 		}
-// 		if (winNumber == attempt) {
-// 			System.out.println("Вы угадали!");
-// 		}
-
-// 		attempt = attempt + 17;
-// 		if (winNumber < attempt) {
-// 			System.out.println("Введенное вами число больше того, что загадал компьютер");
-// 		}
-// 		if (winNumber > attempt) {
-// 			System.out.println("Введенное вами число меньше того, что загадал компьютер");
-// 		}
-// 		if (winNumber == attempt) {
-// 			System.out.println("Вы угадали!");
-// 		}
-// 	}
-// }
