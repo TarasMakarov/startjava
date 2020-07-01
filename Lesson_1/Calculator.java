3cada3de66aeb1dea24467e8a0ffff97c50d3492
Taras Makarov <tarmakar@gmail.com>
2020-07-01 15:23:37 +0300

    implements HW_4

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,33 +1,29 @@
 public class Calculator {
 	public static void main(String[] args) {
 		int a = 8;
-		int b = 0;
-		char operation = '+';
+		int b = 4;
+		int numToPower = 1;
+		char operation = '/';
 
 		if (operation == '+') {
 			System.out.println("Сумма a и b равна: " + (a + b));
 		}
 		
-		operation = '-';
 		if (operation == '-') {
 			System.out.println("Разность a и b равна: " + (a - b));
 		}
 
-		operation = '*';
 		if (operation == '*') {
 			System.out.println("Произведение a и b равно: " + (a * b));
 		}
 
-		operation = '/';
 		if (b != 0 && operation == '/') {
 			System.out.println("Частное a и b равно: " + (a / b));
 		}
-		if (b == 0) {
+		if (operation == '/' && b == 0) {
 			System.out.println("На ноль делить нельзя!");
 		}
 
-		int numToPower = 1;
-		operation = '^';
 		if (operation == '^') {
 			for (int i = 0; i < b; i++) {
 				numToPower *= a;
@@ -35,11 +31,10 @@ public class Calculator {
 			System.out.println("Результат возведения числа a в степень b равен: " + (numToPower));
 		}
 
-		operation = '%';
 		if (b != 0 && operation == '%') {
 			System.out.println("Результат деления по модулю числа a на число b равен: " + (a % b));
 		}
-		if (b == 0) {
+		if (operation == '%' && b == 0) {
 			System.out.println("На ноль делить нельзя!");
 		}
 	} 
