95de8a004a8d8ea4253d9634c46645c71489baa6
Taras Makarov <tarmakar@gmail.com>
2020-07-01 22:29:42 +0300

    implements HW_4

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,39 +1,29 @@
 public class Calculator {
-		public static void main(String[] args) {
-				int a = 8;
-				int b = 0;
-				char operation = '%';
+	public static void main(String[] args) {
+		int a = 8;
+		int b = 5;
+		char operation = '%';
 
-				if (operation == '+') {
-						System.out.println("Сумма a и b равна: " + (a + b));
-				}
-		
-				else if (operation == '-') {
-						System.out.println("Разность a и b равна: " + (a - b));
-				}
-
-				else if (operation == '*') {
-						System.out.println("Произведение a и b равно: " + (a * b));
-				}
-
-				else if (b != 0 && operation == '/') {
-						System.out.println("Частное a и b равно: " + (a / b));
-				}
-
-				else if (operation == '/' && b == 0 || operation == '%' && b == 0) {
-						System.out.println("На ноль делить нельзя!");
-				}
-
-				else if (operation == '^') {
-						int numToPower = 1;
-						for (int i = 0; i < b; i++) {
-								numToPower *= a;
-						}
-						System.out.println("Результат возведения числа a в степень b равен: " + (numToPower));
-				}
-
-				else {
-						System.out.println("Результат деления по модулю числа a на число b равен: " + (a % b));
-				}
-		} 
+		if (operation == '+') {
+			System.out.println("Сумма a и b равна: " + (a + b));
+		} else if (operation == '-') {
+			System.out.println("Разность a и b равна: " + (a - b));
+		} else if (operation == '*') {
+			System.out.println("Произведение a и b равно: " + (a * b));
+		} else if (b != 0 && operation == '/') {
+			System.out.println("Частное a и b равно: " + (a / b));
+		} else if (operation == '/' && b == 0) {
+			System.out.println("На ноль делить нельзя!");
+		} else if (operation == '^') {
+			int numToPower = 1;
+			for (int i = 0; i < b; i++) {
+				numToPower *= a;
+			}
+			System.out.println("Результат возведения числа a в степень b равен: " + (numToPower));
+		} else if (operation == '%' && b == 0) {
+			System.out.println("На ноль делить нельзя!");
+		} else {
+			System.out.println("Результат деления по модулю числа a на число b равен: " + (a % b));
+		}
+	} 
 }
