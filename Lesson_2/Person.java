73cbaf1255b813a116c629400f17ab1f0f2e9e52
Taras Makarov <tarmakar@gmail.com>
2020-07-03 13:07:57 +0300

    implements HW_1

---
+++ Lesson_2/Person.java
@@ -0,0 +1,34 @@
+public class Person {
+
+	String sex = "female";
+	String name = "Evdokiya";
+	int height = 185;
+	int weight = 65;
+	int age = 24;
+
+	void canGo () {
+
+	}
+
+	String canSit () {
+		return "I'll rest a bit";
+	}
+
+	void canRun () {
+
+	}
+
+	void canSpeak () {
+		System.out.println("I am " + name);
+	}
+
+	void canLearnJava () {
+
+	}
+
+}
+/*Создайте класс Person (без метода main)
+Напишите в нем поля (атрибуты), описывающие характеристики человека: пол, имя, рост, вес, возраст
+Проинициализируйте их какими-то значениями
+Создайте в классе методы, описывающие поведение (что он может делать) человека: идти, сидеть, бежать, говорить, учить Java
+Покажите Д/З наставнику*/
