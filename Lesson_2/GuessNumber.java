58eabb03f4ca7ca5f85aea3a7430326526264906
Taras Makarov <tarmakar@gmail.com>
2020-07-15 18:23:35 +0300

    implements HW_4

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -15,18 +15,17 @@ public class GuessNumber {
 
 	public void guessGame() {
 		sayHello();
-		System.out.println("Игра начинается!");
 		compNumber = (int) (Math.random() * 101);
 		System.out.println("Компьютер загадал свое число. И это число: " + compNumber);
 		do {
 			System.out.println(player1.getName() + ", как Вы думаете, какое число загадал компьютер?");
 			player1.setNumber(input.nextInt());
 			System.out.println("Я думаю, это число: " + player1.getNumber());
-			if (compareNums(player1)) {
+			if (!compareNums(player1)) {
 				System.out.println(player2.getName() + ", как Вы думаете, какое число загадал компьютер?");
 				player2.setNumber(input.nextInt());
 				System.out.println("Я думаю, это число: " + player2.getNumber());
-				if (!compareNums(player2)) {
+				if (compareNums(player2)) {
 					break;
 				}
 			} else {
@@ -41,19 +40,18 @@ public class GuessNumber {
 		System.out.println("Игрок под номером 2");
 		System.out.println("Меня зовут " + player2.getName());
 		System.out.println("Сегодня с нами играют: " + player1.getName() + " и " + player2.getName() + " !!!");
+		System.out.println("Игра начинается!");
 	}
 
 	private boolean compareNums(Player player) {
 		if (compNumber == player.getNumber()) {
 			System.out.println(player.getName() + ", Вы угадали!");
 			System.out.println("Игра окончена!");
-			return false;
+			return true;
 		} else {
-			if (compNumber < player.getNumber()) {
-				System.out.println("Введенное вами число больше того, что загадал компьютер");
-			} else {
-				System.out.println("Введенное вами число меньше того, что загадал компьютер");
-			} return true;
-		}
+			String answerCompareNum = compNumber < player.getNumber()? "Введенное вами число больше того, что загадал компьютер" : 
+			"Введенное вами число меньше того, что загадал компьютер";
+			System.out.println(answerCompareNum);
+		} return false;
 	}
 }
